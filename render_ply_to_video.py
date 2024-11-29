ee9f05afba369c1d621a77f57bb321c7a53c1358
PhilipsDeng <philips.deng@yahoo.com>
2024-11-29 02:16:30 +0100

    optimze lighting

--- render_ply_to_video.py
+++ render_ply_to_video.py
@@ -9,7 +9,7 @@ bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete(use_global=False)
 
 
-base_path = "C:/Users/Philips Deng/Desktop/dragon/video1/"
+base_path = "C:/Users/Philips Deng/Desktop/dragon/video3/"
 
 all_files = os.listdir(base_path)
 
@@ -34,13 +34,51 @@ bpy.context.scene.world.use_nodes = True
 bg = bpy.context.scene.world.node_tree.nodes['Background']
 bg.inputs['Color'].default_value = (0, 0, 0, 1)  # 设置背景颜色为纯黑色
 
+def create_area_light(name, location, rotation_euler, energy=12000, size=50):
+    """
+    创建一个AREA光源并将其添加到当前场景中。
 
-top_light_data = bpy.data.lights.new(name="TopAreaLight", type='AREA')
-top_light_data.size = 100
-top_light_data.energy = 100000
-top_light_object = bpy.data.objects.new(name="TopAreaLight", object_data=top_light_data)
-bpy.context.collection.objects.link(top_light_object)
-top_light_object.location = (0, 0, 10)
+    :param name: 光源名称
+    :param location: 光源位置（元组）
+    :param rotation_euler: 光源旋转（弧度）
+    :param energy: 光源能量
+    :param size: 光源大小
+    :return: 创建的光源对象
+    """
+    light_data = bpy.data.lights.new(name=name, type='AREA')
+    light_data.energy = energy
+    light_data.size = size
+
+    light_object = bpy.data.objects.new(name=name, object_data=light_data)
+    bpy.context.collection.objects.link(light_object)
+    light_object.location = location
+    light_object.rotation_euler = rotation_euler
+
+    return light_object
+
+
+# 前部光源
+front_light = create_area_light(
+    name="FrontAreaLight",
+    location=(0, -20, 10),
+    rotation_euler=(math.radians(90), 0, 0)
+)
+
+# 后部光源
+back_light = create_area_light(
+    name="BackAreaLight",
+    location=(0, 20, 10),
+    rotation_euler=(math.radians(-90), 0, 0)
+
+)
+
+# # 底部光源
+# bottom_light = create_area_light(
+#     name="BottomAreaLight",
+#     location=(0, 0, -20),
+#     rotation_euler=(math.radians(180), 0, 0)
+# )
+
 
 
 loaded_objects = []
@@ -72,6 +110,9 @@ for i, (obj_file, texture_file) in enumerate(zip(reversed(obj_files), reversed(t
     tex_image_node.image = bpy.data.images.load(filepath=base_path + texture_file)
     links.new(tex_image_node.outputs['Color'], bsdf_node.inputs['Base Color'])
 
+
+    bsdf_node.inputs['Metallic'].default_value = 0.2
+    bsdf_node.inputs['Roughness'].default_value = 0.8
 
     obj.data.materials.append(mat)
 
@@ -154,7 +195,7 @@ for frame in range(frame_start, frame_end + 1):
     empty.keyframe_insert(data_path="rotation_euler", index=-1)
 
 
-bpy.context.scene.render.filepath = base_path + "animation.mp4"
+bpy.context.scene.render.filepath = base_path + "animation1.mp4"
 bpy.context.scene.render.image_settings.file_format = 'FFMPEG'
 bpy.context.scene.render.ffmpeg.format = 'MPEG4'
 bpy.context.scene.render.ffmpeg.codec = 'H264'
@@ -163,7 +204,7 @@ bpy.context.scene.render.ffmpeg.constant_rate_factor = 'HIGH'
 
 bpy.context.scene.render.resolution_x = 1920  
 bpy.context.scene.render.resolution_y = 1080  
-bpy.context.scene.render.fps = 60  
+bpy.context.scene.render.fps = 30  
 
 
 bpy.ops.render.render(animation=True)
